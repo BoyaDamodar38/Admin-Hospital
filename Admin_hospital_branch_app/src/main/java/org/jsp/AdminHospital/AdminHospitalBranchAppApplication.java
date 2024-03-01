1eb42bbfebb402980e35cbf949a06c0bda546daf
BoyaDamodar38 <boyadamodar38@gmail.com>
2024-03-01 12:03:36 +0530

    added project

---
+++ Admin_hospital_branch_app/src/main/java/org/jsp/AdminHospital/AdminHospitalBranchAppApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.AdminHospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminHospitalBranchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminHospitalBranchAppApplication.class, args);
+	}
+
+}
---
+++ Admin_hospital_branch_app/src/test/java/org/jsp/AdminHospital/AdminHospitalBranchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.AdminHospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminHospitalBranchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
